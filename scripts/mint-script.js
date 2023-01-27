619bac65b1aecca4a83038f2e3c714fedb263174
Mariyan Todorov <mariyan.todorov13@gmail.com>
2023-01-27 15:44:45 +0200

    add deploy script

--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -7,25 +7,21 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const BinaryvilleRobotsNFT = await hre.ethers.getContractFactory("BinaryvilleRobotsNFT");
+  const robotNFT = await BinaryvilleRobotsNFT.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await robotNFT.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `BinaryvilleRobotsNFT deployed to ${robotNFT.address}`
   );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
 });
